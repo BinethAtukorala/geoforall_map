b7b729438f3c255ead41453d89f0d73b26fd548a
BinethAtukorala <bineth.mandiv@gmail.com>
2019-12-10 20:37:05 +0530

    BUGSS

--- app/generate.py
+++ app/generate.py
@@ -1,7 +1,7 @@
 from src import get_data
 import SimpleHTTPServer
 
-generate = input("Do you want to generate new dataset? (y/n): ")
+generate = raw_input("Do you want to generate new dataset? (y/n): ")
 
 if(generate=='y'):
     get_data.main()
