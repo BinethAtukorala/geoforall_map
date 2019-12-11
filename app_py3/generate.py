364548afd25b1aeb214e65e40eff39196e362d53
BinethAtukorala <bineth.mandiv@gmail.com>
2019-12-11 11:59:32 +0530

    Reinstalled libraries

--- app_py3/generate.py
+++ app_py3/generate.py
@@ -1,9 +1,6 @@
 from src import get_data
-import SimpleHTTPServer
 
 generate = input("Do you want to generate new dataset? (y/n): ")
 
 if(generate=='y'):
     get_data.main()
-
-# Implement auto starting server
