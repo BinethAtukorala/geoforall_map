f76d08914d8da6e3fcfb05f228e87bd28e7496d6
BinethAtukorala <bineth.mandiv@gmail.com>
2019-12-11 12:01:26 +0530

    Removed Python 2 program

--- app/generate.py
+++
@@ -1,9 +0,0 @@
-from src import get_data
-import SimpleHTTPServer
-
-generate = raw_input("Do you want to generate new dataset? (y/n): ")
-
-if(generate=='y'):
-    get_data.main()
-
-# Implement auto starting server
